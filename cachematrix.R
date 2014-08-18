2539f512ce1885fc076a5a7d2258975f808bdb40
fxl <frank@frankledo.com>
2014-08-17 17:24:49 -0700

    Fixed sublime settings for R, fixed file.

--- cachematrix.R
+++ cachematrix.R
@@ -7,25 +7,25 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-	m  <- NULL
-	set  <- function(y){
-	        x <<- y
-	        m <<- NULL 
-	}
-	get  <- function() {
-		x
-	}
-	setinverse  <- function(inverse) {
-		m <<- inverse
-	}
-	getinverse  <- function() {
-		m
-	}
-	# Return a list of methods
-	return(list(set = set,
-	       get = get,
-	       setinverse = setinverse,
-	       getinverse = getinverse))
+    m  <- NULL
+    set  <- function(y){
+            x <<- y
+            m <<- NULL
+    }
+    get  <- function() {
+        x
+    }
+    setinverse  <- function(inverse) {
+        m <<- inverse
+    }
+    getinverse  <- function() {
+        m
+    }
+    # Return a list of methods
+    return(list(set = set,
+           get = get,
+           setinverse = setinverse,
+           getinverse = getinverse))
 }
 
 ## This function computes the inverse of the special "matrix" retValurned by
@@ -33,18 +33,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## matrix has not changed), then cacheSolve should retValrieve the inverse from
 ## the cache.
 cacheSolve <- function(x, ...) {
-	## return a matrix that is the inverse of 'x'
-	retVal  <- x$getinverse()
-	# Did we get a cached value?
-	if (!is.null(retVal)){
-		# Yes
-		message("Using cached data.")
-	} else {
-		# No, do the heavy lifting...
-		retVal <- solve(x$get(), ...)
-		x$setinverse(retVal)
-	}
-	# Return the result.  Sure the keyword 'return' is not needed, but it
-	# keeps me sane with I switch languages.
-	return(retVal)
+    ## return a matrix that is the inverse of 'x'
+    retVal  <- x$getinverse()
+    # Did we get a cached value?
+    if (!is.null(retVal)){
+        # Yes
+        message("Using cached data.")
+    } else {
+        # No, do the heavy lifting...
+        retVal <- solve(x$get(), ...)
+        x$setinverse(retVal)
+    }
+    # Return the result.  Sure the keyword 'return' is not needed, but it
+    # keeps me sane with I switch languages.
+    return(retVal)
 }
